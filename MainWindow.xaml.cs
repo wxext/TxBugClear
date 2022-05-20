f1888b0af72cc98bce426792a8aca78180417a7d
myisme <you@example.com>
2022-05-20 16:17:49 +0800

    TxBugReport.exe

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Management;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace TxBugClear
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// https://github.com/wxext/TxBugClear
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DirectoryInfo TxBugReportDir;
+        DispatcherTimer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(3);
+            timer.Tick += Timer_Tick; ;
+            timer.Start();
+
+            var TxBugReportPath= Path.Combine(
+                Path.GetTempPath(),
+                "Tencent"
+                 );
+            TxBugReportDir = new DirectoryInfo(TxBugReportPath);
+            txtLog.Text = "正在监测bug文件...";
+            try
+            {
+                bugStr = File.ReadAllText(".bugStr.ini");
+                bugTime = long.Parse(File.ReadAllText(".bugTime.ini"));
+                txtLog.Text = new DateTime(bugTime).ToString("yyyy-MM-dd HH:mm:ss") + Environment.NewLine + bugStr;
+            }
+            catch (Exception)
+            {
+            }
+        }
+        long bugTime = 0;
+        string bugStr = "";
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            foreach (var item in Process.GetProcessesByName("TxBugReport")) KillProcessAndChildren(item.Id);
+            if (TxBugReportDir.Exists)
+            {
+                foreach (var item in TxBugReportDir.GetFiles("*"))
+                {
+
+                    try
+                    {
+                        if (Regex.IsMatch(item.Name, "^WeChat[0-9a-z]+.txt$"))
+                        {
+                            if (item.CreationTime.Ticks > bugTime)
+                            {
+                                bugTime = item.CreationTime.Ticks;
+                                var arr = File.ReadAllText(item.FullName).Split('\n');
+                                foreach (var str in arr)
+                                {
+                                    if (str.Contains("show in line:")) bugStr = str;
+                                    else if (Regex.IsMatch(str, "^\\[[0-9A-F]+,[0-9A-F]+\\] "))
+                                    {
+                                        var mstr = str.ToUpper();
+                                        if (mstr.Contains("\\SYSTEM")) continue;
+                                        if (mstr.Contains("\\WINDOWS\\")) continue;
+                                        if (mstr.Contains("\\MICROSOFT\\")) continue;
+                                        if (mstr.Contains("\\WECHAT\\"))
+                                        {
+                                            if (!mstr.Contains("\\WECHATWIN")) continue;
+                                        }
+                                        bugStr += str;
+                                        File.WriteAllText(".bugStr.ini", bugStr);
+                                        File.WriteAllText(".bugTime.ini", bugTime.ToString());
+                                    }
+                                }
+                                txtLog.Text = new DateTime(bugTime).ToString("yyyy-MM-dd HH:mm:ss") + Environment.NewLine + bugStr;
+                            }
+                        }
+                        item.Delete();
+                    }
+                    catch (Exception ex)
+                    {
+                        txtLog.Text = ex.Message;
+                    }
+                }
+            }
+        }
+
+        public static void KillProcessAndChildren(int pid)
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("Select * From Win32_Process Where ParentProcessID=" + pid);
+            ManagementObjectCollection moc = searcher.Get();
+            foreach (ManagementObject mo in moc)
+            {
+                KillProcessAndChildren(Convert.ToInt32(mo["ProcessID"]));
+            }
+            try
+            {
+                Process proc = Process.GetProcessById(pid);
+                proc.Kill();
+            }
+            catch (Exception)
+            {
+                /* process already exited */
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # TxBugClear
 微信崩溃时关闭TxBugReport.exe弹出的错误窗口,提取错误信息并清空错误文件
+
+TxBugReport.exe 会记录每次崩溃错误信息
+
+微信每次启动会检查崩溃次数进行相应操作
+
+# 功能
+
+自动关闭 TxBugReport.exe
+
+定时清理崩溃记录
+
+提取最新的崩溃关键信息
